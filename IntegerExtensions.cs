f9b73171d2c5096a312941af50f3a408c8b54268
mxmstfnk <boss.canta@mail.ru>
2019-02-09 21:28:59 +0300

    1

---
+++ IntegerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PadawansTask3
+{
+    public static class IntegerExtensions
+    {
+        public static int Gcd(int a, int b)
+        {
+            // put your code here
+            
+            for (int i = a; i > 0; i--)
+            {
+                if (a % i == 0)
+                {
+                    if (b % i == 0)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            /*
+            return b == 0 ? a : Gcd(b, a % b);*/
+
+            throw new NotImplementedException();
+        }
+    }
+}
